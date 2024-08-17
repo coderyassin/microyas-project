c4063d2a1ca2b3080e6abcdbbc42f20aa7e98fc8
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-08-17 11:47:32 +0100

    feat: add User entity

---
+++ user-service/src/main/java/org/yascode/microyas/user_service/entity/User.java
@@ -0,0 +1,20 @@
+package org.yascode.microyas.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+}

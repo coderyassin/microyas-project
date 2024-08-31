6802b73be2ef3656ee0bec8d635d8a57625c6a12
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-08-31 20:47:44 +0100

    feat: configure service registry

---
+++ gateway-service/src/main/java/org/yascode/microyas/gateway_service/filter/LoggingFilter.java
@@ -0,0 +1,22 @@
+package org.yascode.microyas.gateway_service.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        log.info("Filter executed for query: {}", exchange.getRequest().getURI());
+        return chain.filter(exchange)
+                .doOnSuccess(aVoid -> log.info("Success"));
+    }
+
+
+}

ef98816b1cc515b6c2481489b75b5be2aa43f060
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-09-01 11:18:23 +0100

    feat: configure service registry

--- gateway-service/src/main/java/org/yascode/microyas/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/yascode/microyas/gateway_service/GatewayServiceApplication.java
@@ -3,10 +3,6 @@ package org.yascode.microyas.gateway_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.context.annotation.Bean;
 import org.yascode.microyas.gateway_service.config.LoggingGatewayConfig;
 
 @SpringBootApplication
@@ -15,11 +11,6 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
-
-	@Bean
-	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
-		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
 
 }
